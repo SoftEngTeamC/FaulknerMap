9769c38804ec82406885d6f12a2873d641363f12
GGR <ginagonzalezroundey@gmail.com>
2017-03-27 11:23:18 -0400

    database folder

---
+++ .idea/db/driver.java
@@ -0,0 +1,33 @@
+
+import java.sql.*;
+public class JavaDBExample {
+    public static void main(String[] args) {
+        System.out.println("-------Embedded Java DB Connection Testing --------");
+        try {
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Java DB Driver not found. Add the classpath to your module.");
+            System.out.println("For IntelliJ do the following:");
+            System.out.println("File | Project Structure, Modules, Dependency tab");
+            System.out.println("Add by clicking on the green plus icon on the right of the window");
+            System.out.println("Select JARs or directories. Go to the folder where the Java JDK is installed");
+            System.out.println("Select the folder java/jdk1.8.xxx/db/lib where xxx is the version.");
+            System.out.println("Click OK, compile the code and run it.");
+            e.printStackTrace();
+            return;
+        }
+
+        System.out.println("Java DB driver registered!");
+        Connection connection = null;
+
+        try {
+            // substitute your database name for myDB
+            connection = DriverManager.getConnection("jdbc:derby:myDB;create=true");
+        } catch (SQLException e) {
+            System.out.println("Connection failed. Check output console.");
+            e.printStackTrace();
+            return;
+        }
+        System.out.println("Java DB connection established!");
+    }
+}
