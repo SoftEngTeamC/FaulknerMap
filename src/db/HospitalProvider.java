84983a9f4f2d3b6506c891ad615c67cf2f955dd7
wangyaofeng <yaofengwang729@hotmail.com>
2017-03-31 13:10:04 -0400

    a new class for hospital provider

---
+++ src/db/HospitalProvider.java
@@ -0,0 +1,36 @@
+package db;
+
+/**
+ * Created by wangyaofeng on 3/31/17.
+ */
+public class HospitalProvider {
+
+    private String name;
+    private String title;
+    private String location;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
