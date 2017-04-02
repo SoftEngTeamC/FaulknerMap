b7ee6bff6335deaed47cfc33a06282525cc8117e
Sam Coache <sam.c.coache@gmail.com>
2017-04-02 13:54:01 -0400

    Added DBTestRunner

--- Testing/dbtesting.java
+++ Testing/dbtesting.java
@@ -1,3 +1,17 @@
 /**
  * Created by Sam Coache on 4/2/17.
  */
+
+import db.Driver;
+import db.dbHelper.*;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class dbtesting {
+
+    @Test
+    public void testAdd() {
+        assertEquals(X,Y);
+    }
+}
+
