049af8bc3805c27d9d44aa33ce9ead4100aafadd
pcshingles <pshingletoniv@gmail.com>
2017-04-02 17:23:06 -0400

    Whoops, forgot to make the "add person" screen made the add person view made add person controller class accidentally edited the edit person screen until I realized I was assigned "add person" screen mapped all the view objects to their controller objects in map editor and add person views

--- src/MapEditorController.java
+++ src/MapEditorController.java
@@ -1,8 +1,53 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.util.List;
+
 /**
  * Created by jack on 3/30/17.
  */
 public class MapEditorController implements AdminController {
-    public MapEditorController(){}
+
+    // Back and logout buttons
+    @FXML
+    private Button backBtn;
+    @FXML
+    private Button logoutBtn;
+
+    // Edit Node objects
+    @FXML
+    private ListView<String> editNode_routesConnected;
+    @FXML
+    private ListView<String> editNode_disconnectedRoutes;
+    @FXML
+    private Button editNode_updateBtn;
+    @FXML
+    private Button editNode_removeBtn;
+    @FXML
+    private Button editNode_connectBtn;
+    @FXML
+    private Button editNode_undoBtn;
+    @FXML
+    private TextField editNode_nameField;
+
+    // Edit Route objects
+    @FXML
+    private TextField editRoute_nameField;
+    @FXML
+    private Button editRoute_updateBtn;
+    @FXML
+    private ListView<String> editRoute_startingNodes;
+    @FXML
+    private ListView<String> editRoute_endingNodes;
+    @FXML
+    private ListView<String> editRoute_availableNodes;
+    @FXML
+    private Button editRoute_switchBtn;
+
+
+
 
     public void initialize(){}
     public void back(){}
