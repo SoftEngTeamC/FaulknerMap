ca7a68227b854b3d026e74f5d29528bb75095abb
Sam Coache <sam.c.coache@gmail.com>
2017-04-03 14:38:38 -0400

    Better Test Structure Layout

---
+++ Testing/db/dbClasses/CoordinateTest.java
@@ -0,0 +1,49 @@
+package db.dbClasses;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by sam on 4/3/17.
+ */
+public class CoordinateTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void distanceTo() throws Exception {
+    }
+
+    @Test
+    public void getXpos() throws Exception {
+    }
+
+    @Test
+    public void setXpos() throws Exception {
+    }
+
+    @Test
+    public void getYpos() throws Exception {
+    }
+
+    @Test
+    public void setYpos() throws Exception {
+    }
+
+    @Test
+    public void getZpos() throws Exception {
+    }
+
+    @Test
+    public void setZpos() throws Exception {
+    }
+
+}
---
+++ Testing/db/dbHelpers/NodesHelperTest.java
@@ -0,0 +1,47 @@
+package db.dbHelpers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by sam on 4/3/17.
+ */
+public class NodesHelperTest {
+    @Test
+    public void get() throws Exception {
+    }
+
+    @Test
+    public void addNode() throws Exception {
+    }
+
+    @Test
+    public void updateNode() throws Exception {
+    }
+
+    @Test
+    public void deleteNode() throws Exception {
+    }
+
+    @Test
+    public void getNodeByID() throws Exception {
+    }
+
+    @Test
+    public void getNodeByName() throws Exception {
+    }
+
+    @Test
+    public void getNodes() throws Exception {
+    }
+
+    @Test
+    public void printAllNodes() throws Exception {
+    }
+
+    @Test
+    public void populateTable() throws Exception {
+    }
+
+}
